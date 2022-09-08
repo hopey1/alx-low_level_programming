1caf815717e88373873c5e2ef295f97922ffa58f
hopey1 <hopey555agbaye@gmail.com>
2022-09-08 07:51:27 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,7 +5,7 @@
  *
  * Description: using the main function
  *this program prints "Programming is like building a multilingual puzzle
- *    * Return: 0
+ * Return: 0
  */
 
 int main(void)
@@ -27,7 +27,7 @@ int main(void)
 
 								printf("Size of an int: %ld byte(s)\n", sizeof(i));
 
-									printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+			 					printf("Size of a long int: %ld byte(s)\n", sizeof(li));
 
 										printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
 
