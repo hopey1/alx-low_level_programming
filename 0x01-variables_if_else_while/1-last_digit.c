3e5feb7faff64a3ad29397ea203e03fc3a9dafeb
hopey1 <hopey555agbaye@gmail.com>
2022-09-09 07:18:04 -0700

    commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
 /**
+ *  main - print if the number is postive, zero, or negative
  *
- * * main - print if the number is postive, zero, or negative
- *
- * *
- *
- * * Description: using the main function
- *
- * * this program prints "Programming is positive, zero, or negative
- *
- * * Return: 0
- *
- * */
-
+ *  Description: using the main function
+ *  this program prints "Programming is positive, zero, or negative
+ *  Return: 0
+ **/
 int main(void)
 {
-	char ch;
+char ch;
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
-			putchar(ch);
+putchar(ch);
 	}
-	putchar('\n');
+putchar('\n');
 	return (0);
 }
