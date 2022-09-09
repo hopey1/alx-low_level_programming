1f821cb83be77b82196b5d697b0141f85ab5d3b5
hopey1 <hopey555agbaye@gmail.com>
2022-09-09 08:22:29 -0700

    commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,12 +11,12 @@ int main(void)
 {
 
 char ch;
-	for(ch = 'a' ; ch <= 'z' ; ch++)
+for (ch = 'a' ; ch <= 'z' ; ch++)
 
 {
-putchar(ch);
+	putchar(ch);
 
 }
 putchar('\n');
-	return(0);
+return (0);
 }
