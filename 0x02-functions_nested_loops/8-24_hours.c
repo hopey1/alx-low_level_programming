6bb6327ce9f3596f7b9ca6335777f0bc0c7bfa77
hopey1 <hopey555agbaye@gmail.com>
2022-09-13 09:11:05 -0700

    hello

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,8 +16,8 @@ void jack_bauer(void)
 	{
 	while (minutes <= 59)
 	{	
-
-	mins_remainder = minutes % 10;						        hours_remainder = hours % 10;	
+av
+	hours_remainder = hours % 10;	
 	_putchar(hours / 10 + '0');							_putchar(hours_remainder + '0');						_putchar(':');
 	_putchar(minutes / 10 + '0');							_putchar(mins_remainder + '0');							minutes++;
 	_putchar('\n');									}
