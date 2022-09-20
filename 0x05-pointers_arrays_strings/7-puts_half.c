5b10ea92869cd3989cd1e7ed9a17dbcd42a6d609
hopey1 <hopey555agbaye@gmail.com>
2022-09-20 05:16:10 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -24,12 +24,13 @@ void puts_half(char *str)
  *  Return: the length of the given string
  */
 
-int _strlen(char *s)
+int _strlen (char *s)
 {
 	int len = 0;
 	
-	while (*(s + len) != '\0')
-		len++;
+		while (*(s + len) != '\0')
+			len++;
 
-	return (len);
+
+		return (len);
 }
