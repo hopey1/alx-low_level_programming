43738bd6f057a4226c2504aa852e0469c8e94ff8
hopey1 <hopey555agbaye@gmail.com>
2022-10-03 14:32:13 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,14 +14,14 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int index;
 
-	if (size == o)
+	if (size == 0)
 		return (NULL);
 	array = malloc(sizeof(char) * size);
 
 	if (array == NULL)
 		return (NULL);
 
-	for (index = o; index < size; index++)
+	for (index = 0; index < size; index++)
 		array[index] = c;
 
 	return (array);
