b03962818c17f1a149aaf15181838bdf2723fa84
hopey1 <hopey555agbaye@gmail.com>
2022-10-27 07:15:52 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,44 +1,37 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
 
 /**
  *  binary_to_uint - Converts a binary number to an unsigned int
- *  @b: The binary string to converts
+ *  unsigned int.
+ *  @b: binary.
  *
  *  Return: The positive number converted from a binary
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len = 0, count = 0, sum = 0;
+	unsigned int ui;
+	int len, base_two;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
-	len = _strlen(b);
-	while (len--)
+	ui = o;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len --, base_two = 1; len >= 0; len --, base_two *= 2)
 	{
-		if (b[len] != 48 && b[len] != 49)							return (0);
-												if (b[len] == 49)
-			sum += 1 << count;
 
-		count++;								}
-
-	return (sum);
-}
-
-/**
- *   _strlen - Returns the length of a string
- *   @s: String to count
- *
- *   Return: String length
- */
-int _strlen(const char *s)
-{
-	int c = 0;
-
-	while (s[c])
-		c++;
-
-	return (c);
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+												if (b[len] & 1)
+		{
+			ui += base_two;
+		}
+											}
+		
+	return (ui);
 }
