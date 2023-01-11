279a5046114c9c798af4ee86bf45a91c305fbab5
hopey1 <hopey555agbaye@gmail.com>
2023-01-11 02:16:16 -0800

    few

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,101 +1,41 @@
 #include <stdlib.h>
-
 #include <stdio.h>
-
 #include "hash_tables.h"
 
-
-
 /**
+ *   hash_table_delete - Deletes a hash table
+ *   @ht: The hash table to frees
  *
- *   * hash_table_delete - Deletes a hash table
- *
- *     * @ht: The hash table to frees
- *
- *       *
- *
- *         * Return: Nothing!
- *
- *           */
-
+ *   Return: Nothing!
+ */
 void hash_table_delete(hash_table_t *ht)
-
 {
+	unsigned long int i = 0;
+	hash_node_t *node = NULL, *temp = NULL;
 
-		unsigned long int i = 0;
+	if (ht && ht->size && ht->array)
+	{
+		for (; i < ht->size; ++i)							{
+			node = ht->array[i];
+												if (node)
+		{
+			if (node->next)
+			{
+				node = node->next;
+				while (node)									{											temp = node;									node = node->next;
+					free(temp->key);
+					free(temp->value);								free(temp);								}									}								
+			node = ht->array[i];	
+			if (node->key && node->value)
+			{
+				free(node->key);
+				free(node->value);
+			}
+		}
 
-			hash_node_t *node = NULL, *temp = NULL;
-
-
-
-				if (ht && ht->size && ht->array)
-
-						{
-
-									for (; i < ht->size; ++i)
-
-												{
-
-																node = ht->array[i];
-
-
-
-																			if (node)
-
-																							{
-
-																												if (node->next)
-
-																																	{
-
-																																							node = node->next;
-
-																																												while (node)
-
-																																																		{
-
-																																																									temp = node;
-
-																																																															node = node->next;
-
-																																																																					free(temp->key);
-
-																																																																											free(temp->value);
-
-																																																																																	free(temp);
-
-																																																																																						}
-
-																																																}
-
-
-
-																																node = ht->array[i];
-
-																																				if (node->key && node->value)
-
-																																									{
-
-																																															free(node->key);
-
-																																																				free(node->value);
-
-																																																								}
-
-																																							}
-
-
-
-																						free(node);
-
-																								}
-
-
-
+		free(node);
+	}
 											free(ht->array);
-
-													free(ht);
-
-														}
-
+	free(ht);
+	}
 }
